e6c1f9e45bc16b215fc86b61a1ab158154082e36
Varrick1 <tckrasnai@gmail.com>
2025-08-17 18:55:08 -0400

    uwu

--- server_scripts/blood.js
+++ server_scripts/blood.js
@@ -1,4 +1,5 @@
 ServerEvents.tags('item', event => {
     event.add('fh:blood', 'fh:blood_bottle')
     event.add('swordblockingmechanics:can_perform_sword_blocking', '#fh:twohanded')
+    event.remove('swordblockingmechanics:can_perform_sword_blocking', '#minecraft:swords')
 })
